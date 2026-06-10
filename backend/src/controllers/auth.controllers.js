bd069d90eea0140fbeb0c9d3aa15dfd149815f65
Tamim Bro <tamimbro103@gmail.com>
2026-06-09 23:12:30 -0800

    Update auth.controllers.js

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -23,7 +23,7 @@ const googleAuth = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 2 * 24 * 60 * 60 * 1000, 
-      sameSite: "strict",
+      sameSite: "none",
       secure: process.env.NODE_ENV === "production",
     });
 
